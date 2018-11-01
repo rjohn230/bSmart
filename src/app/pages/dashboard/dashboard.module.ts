a5ad8dab49620b35c2d5ff80e3ccfe1830822439
Rom Profesorsky <rprofesorsky050@hotmail.com>
2018-11-01 15:06:50 -0400

    Stripped site of unnecessary tools

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -8,18 +8,11 @@ import { StatusCardComponent } from './status-card/status-card.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { RoomSelectorComponent } from './rooms/room-selector/room-selector.component';
-import { TemperatureComponent } from './temperature/temperature.component';
-import { TemperatureDraggerComponent } from './temperature/temperature-dragger/temperature-dragger.component';
 import { TeamComponent } from './team/team.component';
 import { KittenComponent } from './kitten/kitten.component';
-import { SecurityCamerasComponent } from './security-cameras/security-cameras.component';
 import { ElectricityComponent } from './electricity/electricity.component';
 import { ElectricityChartComponent } from './electricity/electricity-chart/electricity-chart.component';
-import { WeatherComponent } from './weather/weather.component';
-import { SolarComponent } from './solar/solar.component';
 import { PlayerComponent } from './rooms/player/player.component';
-import { TrafficComponent } from './traffic/traffic.component';
-import { TrafficChartComponent } from './traffic/traffic-chart.component';
 
 
 @NgModule({
@@ -30,21 +23,14 @@ import { TrafficChartComponent } from './traffic/traffic-chart.component';
   declarations: [
     DashboardComponent,
     StatusCardComponent,
-    TemperatureDraggerComponent,
     ContactsComponent,
     RoomSelectorComponent,
-    TemperatureComponent,
     RoomsComponent,
     TeamComponent,
     KittenComponent,
-    SecurityCamerasComponent,
     ElectricityComponent,
     ElectricityChartComponent,
-    WeatherComponent,
     PlayerComponent,
-    SolarComponent,
-    TrafficComponent,
-    TrafficChartComponent,
   ],
 })
 export class DashboardModule { }
