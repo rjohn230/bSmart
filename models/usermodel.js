58dba42a025db8970309d92cabca7b24ad0c93fb
rshockle@uncc.edu <rshockle@uncc.edu>
2018-11-05 16:08:26 -0500

    started models file with a basic user schema

---
+++ models/usermodel.js
@@ -0,0 +1,23 @@
+//TODO: model for making entires in the database
+//stuff in here subject to change
+//model file creates requests to database via mongoose. It is also responsible
+//for functions like looking up users by their database entries EX: username, email
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = mongoose.Schema ({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  }
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
