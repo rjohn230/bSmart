7f1e15566f0d3e232eb3dece00fa8e887153834b
rshockle@uncc.edu <rshockle@uncc.edu>
2018-11-05 16:14:46 -0500

    started skeleton for route file

---
+++ routes/api/route.js
@@ -0,0 +1,11 @@
+//TODO: express router goes here and exports
+//handles GET, POST, PUT, DELETE requests
+const express = require('express');
+
+const router = express.Router();
+
+
+
+
+
+module.exports = router;
