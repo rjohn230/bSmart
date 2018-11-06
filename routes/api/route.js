efb803cca367efaa0eafbb552136bf59d8561b73
Robert Shockley <rshockle@gmail.com>
2018-11-06 17:42:34 -0500

    commented get, post, put, delete skeletons w/ some basic syntax

--- routes/api/route.js
+++ routes/api/route.js
@@ -4,8 +4,22 @@ const express = require('express');
 
 const router = express.Router();
 
-
-
-
-
+//Basic syntax for how this goes, request, response.
+//function is a placeholder name along with the chevrons(<>)
+//GET
+router.get('/', <function (req, res)>){
+  res.send()
+}
+//POST
+router.post('/',<function (req, res)>){
+  res.send()
+}
+//PUT
+router.put('/', <function (req, res)>){
+  res.send()
+}
+//DELETE
+router.delete('/', <function (req, res)>){
+  res.send()
+}
 module.exports = router;
