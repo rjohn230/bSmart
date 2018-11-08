2c43f601b3a4b807f3ff10e7858af23c17424228
Rob <rshockle@gmail.com>
2018-11-08 15:51:56 -0500

    basic functions headers written, need to look at the angular routing to determine what routing express needs to do

--- routes/api/route.js
+++ routes/api/route.js
@@ -1,25 +1,29 @@
-//TODO: express router goes here and exports
-//handles GET, POST, PUT, DELETE requests
+/*
+TODO: express router goes here and exports
+handles GET, POST, PUT, DELETE requests
+*/
 const express = require('express');
 
 const router = express.Router();
 
-//Basic syntax for how this goes, request, response.
-//function is a placeholder name along with the chevrons(<>)
+/*
+Basic syntax for how this goes, request, response.
+function is a placeholder name along with the chevrons(<>)
+*/
 //GET
-router.get('/', <function (req, res)>){
+router.get('/', function (req, res)){
   res.send()
 }
 //POST
-router.post('/',<function (req, res)>){
+router.post('/', function (req, res)){
   res.send()
 }
 //PUT
-router.put('/', <function (req, res)>){
+router.put('/', function (req, res)){
   res.send()
 }
 //DELETE
-router.delete('/', <function (req, res)>){
+router.delete('/', function (req, res)){
   res.send()
 }
 
