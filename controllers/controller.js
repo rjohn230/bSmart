74cb95cef8e55ed5e0f2e7dc5a564ba828dd3a12
Rob <rshockle@gmail.com>
2018-11-08 15:50:02 -0500

    linked tutorial in header comment

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,2 +1,3 @@
 //TODO: Write me!
 //Interacts with model file to create requests to database via stuff on webpage
+//https://medium.com/netscape/mean-app-tutorial-with-angular-4-part-1-18691663ea96
