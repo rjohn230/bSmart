fa6e30b92b52db1e2b039f064eb0fd1967c9fc45
Rom Profesorsky <rprofesorsky050@hotmail.com>
2018-11-26 20:21:33 -0500

    Added plaid link button

--- src/app/pages/e-commerce/plaid-button/plaid-button.component.ts
+++ src/app/pages/e-commerce/plaid-button/plaid-button.component.ts
@@ -1,18 +1,6 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import {
-  PlaidErrorMetadata,
-  PlaidErrorObject,
-  PlaidEventMetadata,
-  PlaidOnEventArgs,
-  PlaidOnExitArgs,
-  PlaidOnSuccessArgs,
-  PlaidSuccessMetadata,
-  PlaidConfig
-} from 'ngx-plaid-link/lib/interfaces';
-import { NgxPlaidLinkService } from 'ngx-plaid-link/lib/ngx-plaid-link.service';
-import { PlaidLinkHandler } from 'ngx-plaid-link/lib/ngx-plaid-link-handler';
- 
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'ngx-plaid-button',
@@ -20,81 +8,95 @@ import { PlaidLinkHandler } from 'ngx-plaid-link/lib/ngx-plaid-link-handler';
   styleUrls: ['./plaid-button.component.scss']
 })
 export class PlaidButtonComponent implements OnInit {
+  linkHandler;
+  
+  themeName = 'default';
+  settings: Array<any>;
+  themeSubscription: Subscription;
 
-  constructor() { }
+  constructor(private themeService: NbThemeService) {
+    this.themeSubscription = this.themeService.getJsTheme().subscribe(theme => {
+      this.themeName = theme.name;
+      this.init(theme.variables);
+    });
+    this.linkHandler = Plaid.create({
+      clientName: 'bSmart',
+      env: 'development',
+      apiVersion: 'v2',
+      key: '5b90ad3428575376ee2637218f0009',
+      product: ['auth', 'transactions'],
+      webhook: '',
+      selectAccount: true,
+      forceIframe: true,
+      onLoad: function() {
+        console.log('loaded');
+      },
+      onSuccess: function(public_token, metadata) {
+        console.log(public_token);
+        console.log(metadata.account.id, metadata.account.name);
+      },
+      onExit: function(err, metadata) {
+        if (err != null) {
+          console.log(err);
+        }
+        console.log(metadata);
+        console.log(metadata.institution.name, metadata.institution.institution_id);
+        console.log(metadata.request_id)
+      }
+    });
+  }
+
+  init(colors: any) {
+    this.settings = [
+      {
+        status: 'primary',
+        container: 'primary-container',
+        title: 'Plaid Connect',
+        buttonTitle: 'Connect your bank account!',
+        default: {
+          gradientLeft: `adjust-hue(${colors.primary}, 20deg)`,
+          gradientRight: colors.primary,
+        },
+        corporate: {
+          color: colors.primary,
+          glow: {
+            params: '0 0 20px 0',
+            color: 'rgba (115, 161, 255, 0.5)',
+          },
+        },
+        cosmic: {
+          gradientLeft: `adjust-hue(${colors.primary}, 20deg)`,
+          gradientRight: colors.primary,
+          bevel: `shade(${colors.primary}, 14%)`,
+          shadow: 'rgba (6, 7, 64, 0.5)',
+          glow: {
+            params: '0 2px 12px 0',
+            color: `adjust-hue(${colors.primary}, 10deg)`,
+          },
+        },
+      },
+    ];
+  }
+
+  ngOnDestroy() {
+    this.themeSubscription.unsubscribe();
+  }
+
+  forceExitLink() {
+    this.linkHandler.exit();
+  }
+
+  openLink() {
+    this.linkHandler.open();
+  }
+
+  openLinkWithInstitution() {
+    this.linkHandler.open('ins_4');
+  }
 
   ngOnInit() {
   }
 
-  onPlaidSuccess(event) {
-    // Send the public token to your server so you can do the token exchange.
-  }
 
-  onPlaidExit(event) {
-    // Get errors or exit reason.
-  }
 
-  onPlaidEvent(event) {
-    // Log events so you can have insight into how your users are using plaid link.
-  }
-
-  onPlaidLoad(event) {
-    // Do something when the iframe loads.
-  }
-
-  onPlaidClick(event) {
-    // Do something when the button is clicked.
-  }
-
-}
-
-export class ComponentThatImplementsPlaidLink implements AfterViewInit {
-  private plaidLinkHandler: PlaidLinkHandler;
- 
-  private config: PlaidConfig = {
-    apiVersion: 'v2',
-    env: 'sandbox',
-    institution: null,
-    token: null,
-    webhook: '',
-    product: ['auth'],
-    key: 'YOURPUBLICKEY'
-  };
- 
-  constructor(private plaidLinkService: NgxPlaidLinkService) {}
- 
-  // Create and open programatically once the library is loaded.
-  ngAfterViewInit() {
-    this.plaidLinkHandler.createPlaid(Object.assign({}, this.config, {
-      onSuccess: (token, metadata) => this.onSuccess(token, metadata),
-      onExit: (error, metadata) => this.onExit(error, metadata),
-      onEvent: (eventName, metadata) => this.onEvent(eventName, metadata)
-    })).then((handler: PlaidLinkHandler) => {
-      this.plaidLinkHandler = handler;
-      this.open();
-    });
-  }
- 
-  open() {
-    this.plaidLinkHandler.open();
-  }
- 
-  exit() {
-    this.plaidLinkHandler.exit();
-  }
- 
-  onSuccess(token, metadata) {
-    console.log("We got a token:", token);
-    console.log("We got metadata:", metadata);
-  }
- 
-  onEvent(eventName, metadata) {
-    console.log("We got an event:", eventName);
-    console.log("We got metadata:", metadata);
-  }
- 
-  onExit(error, metadata) {
-    console.log("We exited:", error);
-    console.log("We got metadata:", metadata);
-  }
 }
