7a48896206c4890adcfc1ec56fbaa316935ec314
Robert Shockley <rshockle@gmail.com>
2018-11-28 15:11:48 -0500

    removed files that don't want to stay in the backend folder

--- config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
- //database: 'mongodb://<dbuser>:<dbpassword>@ds151943.mlab.com:51943/bsmart_db ',,   //prod
-  database: 'mongodb://localhost:27017/meanauth',    //dev
-  //secret: 'yoursecret'
-}
