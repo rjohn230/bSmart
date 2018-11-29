2386da17f992a8063664c7de375ef1ec72e3297c
Rom Profesorsky <rprofesorsky050@hotmail.com>
2018-11-29 15:36:19 -0500

    PLAID FOOKIN WORKS

--- src/app/pages/e-commerce/plaid-button/plaid-button.component.ts
+++ src/app/pages/e-commerce/plaid-button/plaid-button.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NbThemeService } from '@nebular/theme';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'ngx-plaid-button',
@@ -9,31 +8,20 @@ import { Subscription } from 'rxjs';
 })
 export class PlaidButtonComponent implements OnInit {
   linkHandler;
-  
-  themeName = 'default';
-  settings: Array<any>;
-  themeSubscription: Subscription;
 
-  constructor(private themeService: NbThemeService) {
-    this.themeSubscription = this.themeService.getJsTheme().subscribe(theme => {
-      this.themeName = theme.name;
-      this.init(theme.variables);
-    });
+  constructor() {
+    
     this.linkHandler = Plaid.create({
       clientName: 'bSmart',
       env: 'development',
-      apiVersion: 'v2',
       key: '5b90ad3428575376ee2637218f0009',
       product: ['auth', 'transactions'],
-      webhook: '',
-      selectAccount: true,
-      forceIframe: true,
       onLoad: function() {
         console.log('loaded');
       },
       onSuccess: function(public_token, metadata) {
         console.log(public_token);
-        console.log(metadata.account.id, metadata.account.name);
+        console.log(JSON.stringify(metadata));
       },
       onExit: function(err, metadata) {
         if (err != null) {
@@ -46,41 +34,6 @@ export class PlaidButtonComponent implements OnInit {
     });
   }
 
-  init(colors: any) {
-    this.settings = [
-      {
-        status: 'primary',
-        container: 'primary-container',
-        title: 'Plaid Connect',
-        buttonTitle: 'Connect your bank account!',
-        default: {
-          gradientLeft: `adjust-hue(${colors.primary}, 20deg)`,
-          gradientRight: colors.primary,
-        },
-        corporate: {
-          color: colors.primary,
-          glow: {
-            params: '0 0 20px 0',
-            color: 'rgba (115, 161, 255, 0.5)',
-          },
-        },
-        cosmic: {
-          gradientLeft: `adjust-hue(${colors.primary}, 20deg)`,
-          gradientRight: colors.primary,
-          bevel: `shade(${colors.primary}, 14%)`,
-          shadow: 'rgba (6, 7, 64, 0.5)',
-          glow: {
-            params: '0 2px 12px 0',
-            color: `adjust-hue(${colors.primary}, 10deg)`,
-          },
-        },
-      },
-    ];
-  }
-
-  ngOnDestroy() {
-    this.themeSubscription.unsubscribe();
-  }
 
   forceExitLink() {
     this.linkHandler.exit();
