1cc4798d774242f390d20dea55b96d83417545b2
Robert Shockley <rshockle@gmail.com>
2018-11-30 00:05:32 -0500

    removed PLF route

--- backend/routes/apiroute.js
+++ backend/routes/apiroute.js
@@ -4,12 +4,10 @@ var router = express.Router()
 var Users = require('./api/route-users')
 var Transactions = require('./api/route-transactions')
 var MBs = require('./api/route-MB')
-var PLFs = require('./api/route-PLF')
 
 
 router.use('/user', Users);
 router.use('/transactions', Transactions)
 router.use('/MB', MBs)
-router.use('/PLF', PLFs)
 
 module.exports = router;
