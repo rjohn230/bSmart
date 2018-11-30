aa97bd0845e4a7c046fde2335f74638bcb6f98c7
Robert Shockley <rshockle@gmail.com>
2018-11-30 00:07:29 -0500

    added specific messages so it doesn't look like the app isn't loading anything

--- backend/routes/usersroute.js
+++ backend/routes/usersroute.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a user resource');
 });
+
+/*post
+router.post('/', function(req, res, next){
+  username: req.body.username,
+  email: req.body.email,
+  password: req.body.password
+});
+*/
 
 module.exports = router;
