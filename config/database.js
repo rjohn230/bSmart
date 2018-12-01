8d34f203fd506f9f3728a555c86d1bdfb204659d
Robert Shockley <rshockle@gmail.com>
2018-12-01 16:39:45 -0500

    played with some database stuff, uses 'bSmart' currently

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
- //database: 'mongodb://<dbuser>:<dbpassword>@ds151943.mlab.com:51943/bsmart_db ',,   //prod
-  database: 'mongodb://localhost:27017/bSmart-dev-ML',    //dev
+  //database: 'mongodb://<dbuser>:<dbpassword>@ds151943.mlab.com:51943/bsmart_db ',,   //prod
+  database: 'mongodb://localhost:27017/bSmart',    //dev
   //secret: 'yoursecret'
 }
