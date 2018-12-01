27ca56e64616db38691083b74592fabe37045060
Robert Shockley <rshockle@gmail.com>
2018-12-01 16:47:44 -0500

    Hardcoded/moved api/ directory routes here
    This put all the routing in one file which makes it work,
    but it's pretty rough
    
    currently having issues with all transaction methods, and user create and update
    
    user get and delete are functioning, however delete gives a 400 error, but actually deletes stuff

--- routes/apiroute.js
+++ routes/apiroute.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express')
 
 var router = express.Router()
@@ -5,8 +6,28 @@ var Users = require('./api/route-users')
 var Transactions = require('./api/route-transactions')
 
 
-router.use('/user', Users);
-router.use('/transactions', Transactions)
+router.use('/api/user', Users);
+router.use('/api/transactions', Transactions)
 
+
+module.exports = router;
+*/
+var express = require('express')
+
+var router = express.Router()
+
+var UserController = require('../controllers/usercontroller');
+
+router.get('/u', UserController.getUsers)           //Works
+router.post('/u', UserController.createUsers)       //Does not like line 21 of user controller
+router.put('/u', UserController.updateUsers)
+router.delete('/u/:id',UserController.removeUsers)  //works, but gives the 400 error and says it doesn't, check userserive line 67
+
+var TransactionController = require('../controllers/transactioncontroller');
+
+router.get('/t', TransactionController.getTransactions)
+router.post('/t', TransactionController.createTransactions)
+router.put('/t', TransactionController.updateTransactions)
+router.delete('/t/:id',TransactionController.removeTransactions)
 
 module.exports = router;
