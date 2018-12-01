a5a0151e1c68f9c20b9899fca597bafa1076a6b5
Robert Shockley <rshockle@gmail.com>
2018-12-01 16:43:10 -0500

    changed the response on connect to say transactions

--- routes/transactionsroute.js
+++ routes/transactionsroute.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('This is the transactionsroute!');
 });
 
 module.exports = router;
