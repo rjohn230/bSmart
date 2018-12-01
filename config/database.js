9c44dcaaed4ffa0d91f029e499ee3c57c3095872
Robert Shockley <rshockle@gmail.com>
2018-12-01 11:58:36 -0500

    Changed from obvious copy paste from tutorial

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
  //database: 'mongodb://<dbuser>:<dbpassword>@ds151943.mlab.com:51943/bsmart_db ',,   //prod
-  database: 'mongodb://localhost:27017/meanauth',    //dev
+  database: 'mongodb://localhost:27017/bSmart-dev-ML',    //dev
   //secret: 'yoursecret'
 }
