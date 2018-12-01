773f085c1bf6f892308313854af2130b6f94913e
Robert Shockley <rshockle@gmail.com>
2018-12-01 11:39:03 -0500

    added some 's's

--- backend/routes/api/route-users.js
+++ backend/routes/api/route-users.js
@@ -4,9 +4,9 @@ var router = express.Router()
 
 var UserController = require('../../controllers/usercontroller');
 
-router.get('/user', UserController.getUsers)
-router.post('/user', UserController.createUsers)
-router.put('/user', UserController.updateUsers)
-router.delete('/user/:id',UserController.removeUsers)
+router.get('/users', UserController.getUsers)
+router.post('/users', UserController.createUsers)
+router.put('/users', UserController.updateUsers)
+router.delete('/users/:id',UserController.removeUsers)
 
 module.exports = router;
