f6fb0287dcb670e51f3ecbf6f1535e6b51478005
Robert Shockley <rshockle@gmail.com>
2018-12-06 17:13:38 -0500

    voodoo magic

--- src/app/@core/data/plaid.service.ts
+++ src/app/@core/data/plaid.service.ts
@@ -12,6 +12,21 @@ export class Plaid {
 export class PlaidService {
 
   constructor(client: Client) {
-    
+/*
+   client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
+    if (error != null) {
+      //prettyPrintResponse(error);
+      //return response.json({
+        //error: error,
+     // });
+    }
+    ACCESS_TOKEN = tokenResponse.access_token;
+    ITEM_ID = tokenResponse.item_id;
+        //prettyPrintResponse(tokenResponse);
+
+        //setPredictions(1,request,response,next)
+
+         }); 
+*/
   }
 }
